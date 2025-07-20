c5c98905c6253bf4ae6afe95cf3af52049b7baae
Gibeom Nam <rlqja0523@naver.com>
2025-07-20 21:07:07 +0200

    feat: add LocalDateUtil

---
+++ src/main/kotlin/oncall/util/LocalDateUtil.kt
@@ -0,0 +1,59 @@
+package oncall.util
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+private const val DEFAULT_YEAR = 2023
+private val publicHolidays = setOf(
+    LocalDate.of(DEFAULT_YEAR, 1, 1),
+    LocalDate.of(DEFAULT_YEAR, 3, 1),
+    LocalDate.of(DEFAULT_YEAR, 5, 5),
+    LocalDate.of(DEFAULT_YEAR, 8, 15),
+    LocalDate.of(DEFAULT_YEAR, 10, 3),
+    LocalDate.of(DEFAULT_YEAR, 10, 9),
+    LocalDate.of(DEFAULT_YEAR, 12, 25),
+)
+private val dayOfWeekToKor = mapOf(
+    DayOfWeek.MONDAY to "월",
+    DayOfWeek.TUESDAY to "화",
+    DayOfWeek.WEDNESDAY to "수",
+    DayOfWeek.THURSDAY to "목",
+    DayOfWeek.FRIDAY to "금",
+    DayOfWeek.SATURDAY to "토",
+    DayOfWeek.SUNDAY to "일",
+)
+private val KorToDayOfWeek = dayOfWeekToKor.entries
+    .associate { (k, v) -> v to k }
+
+fun LocalDate.isWorkingDay(): Boolean {
+    return this.dayOfWeek.value in 1..5 && !this.isPublicHoliday()
+}
+
+fun LocalDate.isNonWorkingDay(): Boolean {
+    return this.dayOfWeek.value in 6..7 || this.isPublicHoliday()
+}
+
+fun LocalDate.isPublicHoliday(): Boolean {
+    return publicHolidays.contains(this)
+}
+
+fun LocalDate.korDayOfWeek(): String {
+    return dayOfWeekToKor[this.dayOfWeek]!!
+}
+
+fun LocalDate.next(): LocalDate {
+    return this.plusDays(1)
+}
+
+object LocalDateUtil {
+    fun resolveFirstMatchingDayOfWeek(month: Int, targetDayOfWeek: DayOfWeek): LocalDate {
+        val firstDayOfMonth = LocalDate.of(DEFAULT_YEAR, month, 1)
+        val dayOfWeekDiff = (targetDayOfWeek.value - firstDayOfMonth.dayOfWeek.value + 7) % 7
+        return firstDayOfMonth.plusDays(dayOfWeekDiff.toLong())
+    }
+
+    fun getDayOfWeekFromKor(korDayOfWeek: String): DayOfWeek {
+        return KorToDayOfWeek[korDayOfWeek]
+            ?: throw IllegalArgumentException("Invalid Korean day of week: $korDayOfWeek")
+    }
+}
---
+++ src/test/kotlin/oncall/util/LocalDateUtilTest.kt
@@ -0,0 +1,169 @@
+package oncall.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+class LocalDateUtilTest {
+    @ParameterizedTest(name = "dayOfMonth={0}")
+    @ValueSource(ints = [1, 3])
+    fun `should return false when the date is a weekend or special day`(dayOfMonth: Int) {
+        // given
+        val localDate = LocalDate.of(2023, 10, dayOfMonth)
+
+        // when
+        val isWorkingDay = localDate.isWorkingDay()
+
+        // then
+        assertThat(isWorkingDay).isFalse()
+    }
+
+    @Test
+    fun `should return true when the date is a weekday and not public holiday`() {
+        // given
+        val localDate = LocalDate.of(2023, 10, 2)
+
+        // when
+        val isWorkingDay = localDate.isWorkingDay()
+
+        // then
+        assertThat(isWorkingDay).isTrue()
+    }
+
+    @Test
+    fun `should return false when the date is a weekday`() {
+        // given
+        val localDate = LocalDate.of(2023, 10, 2)
+
+        // when
+        val isWorkingDay = localDate.isNonWorkingDay()
+
+        // then
+        assertThat(isWorkingDay).isFalse()
+    }
+
+    @ParameterizedTest(name = "dayOfMonth={0}")
+    @ValueSource(ints = [1, 3])
+    fun `should return true when the date is a weekend or public holiday`() {
+        // given
+        val localDate = LocalDate.of(2023, 10, 3)
+
+        // when
+        val isNonWorkingDay = localDate.isNonWorkingDay()
+
+        // then
+        assertThat(isNonWorkingDay).isTrue()
+    }
+
+    @Test
+    fun `should return false when the date is not a public holiday`() {
+        // given
+        val localDate = LocalDate.of(2023, 10, 2)
+
+        // when
+        val isPublicHoliday = localDate.isPublicHoliday()
+
+        // then
+        assertThat(isPublicHoliday).isFalse()
+    }
+
+    @Test
+    fun `should return true when the date is a public holiday`() {
+        // given
+        val localDate = LocalDate.of(2023, 10, 3)
+
+        // when
+        val isPublicHoliday = localDate.isPublicHoliday()
+
+        // then
+        assertThat(isPublicHoliday).isTrue()
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2023-10-02, 월",
+        "2023-10-03, 화",
+        "2023-10-04, 수",
+        "2023-10-05, 목",
+        "2023-10-06, 금",
+        "2023-10-07, 토",
+        "2023-10-08, 일"
+    )
+    fun `should return the correct Korean day of the week`(date: String, expectedKorDayOfWeek: String) {
+        // given
+        val localDate = LocalDate.parse(date)
+
+        // when
+        val korDayOfWeek = localDate.korDayOfWeek()
+
+        // then
+        assertThat(korDayOfWeek).isEqualTo(expectedKorDayOfWeek)
+    }
+
+    @Test
+    fun `should return the next day correctly`() {
+        // given
+        val localDate = LocalDate.of(2023, 10, 2)
+
+        // when
+        val nextDate = localDate.next()
+
+        // then
+        assertThat(nextDate).isEqualTo(LocalDate.of(2023, 10, 3))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "10, TUESDAY, 2023-10-03",
+        "11, MONDAY, 2023-11-06",
+        "12, FRIDAY, 2023-12-01",
+    )
+    fun `should resolve the first matching day of the week for a given month`(
+        month: Int,
+        targetDayOfWeek: DayOfWeek,
+        expectedDate: String,
+    ) {
+        // when
+        val firstMatchingDate = LocalDateUtil.resolveFirstMatchingDayOfWeek(month, targetDayOfWeek)
+
+        // then
+        assertThat(firstMatchingDate).isEqualTo(LocalDate.parse(expectedDate))
+    }
+
+    @Test
+    fun `should fail to get day of week from invalid Korean day of week`() {
+        // given
+        val invalidKorDayOfWeek = "월월"
+
+        // when & then
+        assertThatThrownBy { LocalDateUtil.getDayOfWeekFromKor(invalidKorDayOfWeek) }
+            .isExactlyInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Invalid Korean day of week: $invalidKorDayOfWeek")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "월, MONDAY",
+        "화, TUESDAY",
+        "수, WEDNESDAY",
+        "목, THURSDAY",
+        "금, FRIDAY",
+        "토, SATURDAY",
+        "일, SUNDAY"
+    )
+    fun `should succeed in getting day of week from valid Korean day of week`(
+        korDayOfWeek: String,
+        expectedDayOfWeek: DayOfWeek,
+    ) {
+        // when
+        val dayOfWeek = LocalDateUtil.getDayOfWeekFromKor(korDayOfWeek)
+
+        // then
+        assertThat(dayOfWeek).isEqualTo(expectedDayOfWeek)
+    }
+}
