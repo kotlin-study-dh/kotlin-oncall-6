e338c0e5d644ab0331ac5a623917be74c43f6639
Gibeom Nam <rlqja0523@naver.com>
2025-07-21 00:38:52 +0200

    feat: add OutputView

---
+++ src/main/kotlin/oncall/worker/OutputView.kt
@@ -0,0 +1,25 @@
+package oncall.worker
+
+import oncall.util.isPublicHoliday
+import oncall.util.korDayOfWeek
+import java.time.LocalDate
+
+object OutputView {
+    private const val ERROR_MESSAGE_PREFIX = "[ERROR] "
+    private const val PUBLIC_HOLIDAY = "(휴일)"
+
+    fun printSchedule(schedule: Map<LocalDate, Worker>) {
+        schedule.forEach { (date, worker) ->
+            val korDayOfWeek = if (date.isPublicHoliday()) {
+                date.korDayOfWeek() + PUBLIC_HOLIDAY
+            } else {
+                date.korDayOfWeek()
+            }
+            println("${date.month}월 ${date.dayOfMonth}일 $korDayOfWeek ${worker.name}")
+        }
+    }
+
+    fun printError(message: String?) {
+        println("$ERROR_MESSAGE_PREFIX$message")
+    }
+}
