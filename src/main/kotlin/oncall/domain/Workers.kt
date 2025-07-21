2150585ab523bfc4586bff2cebc05d8c25ebb5bb
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-07-21 17:19:20 +0200

    feat: add method to assign worker

--- src/main/kotlin/oncall/domain/Workers.kt
+++ src/main/kotlin/oncall/domain/Workers.kt
@@ -1,6 +1,6 @@
 package oncall.domain
 
-data class Workers(val workers: ArrayDeque<Worker>) {
+class Workers(val workers: ArrayDeque<Worker>) {
 
     init {
         require(workers.size in 5..35) {
@@ -9,6 +9,28 @@ data class Workers(val workers: ArrayDeque<Worker>) {
         require(workers.map { it.name }.distinct().size == workers.size) {
             throw IllegalArgumentException("worker's name must be distinct")
         }
+    }
+
+    fun getAssignedWorkerForDay(lastAssignedWorker: Worker?): Worker {
+        val candidate = workers.firstOrNull() ?: throw IllegalStateException("Worker must not be empty")
+
+        if (candidate == lastAssignedWorker) {
+            val consecutiveWorker = workers.removeFirst()
+            workers.addLast(consecutiveWorker)
+
+            val nextWorker = getNextWorkerAndRotate()
+            val temporaryWorker = Worker(consecutiveWorker.name, true)
+            workers.addFirst(temporaryWorker)
+            return nextWorker
+        } else {
+            return getNextWorkerAndRotate()
+        }
+    }
+
+    fun getNextWorkerAndRotate(): Worker {
+        val worker = workers.removeFirstOrNull() ?: throw IllegalStateException("Worker must not be empty")
+        if(!worker.isTemporary) workers.addLast(worker)
+        return worker
     }
 
     companion object {
